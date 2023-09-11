a2790ae149d45d42f19ab31f51e64480a5b2572a
MehrinFirdousi <mehrinfirdousi@gmail.com>
2023-09-11 21:20:16 +0530

    Final fixes

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -8,7 +8,7 @@ mkdir -p $WPPATH
 $WP core download
 $WP config create --dbhost=mariadb:3306 --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
 $WP core install --url=$DOMAIN --title="My WordPress Site" --admin_name=$WP_ADMIN --admin_password=$WP_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-$WP user create jensown jensown@mehrin.com --role=contributor --user_pass=$NEW_USER_PASS
+$WP user create $NEW_USER_NAME $NEW_USER_EMAIL --role=contributor --user_pass=$NEW_USER_PASS
 
 chown -R www-data:www-data $WPPATH
 
